594a4bde53ccaa8d92a4b28bcd31b5f5c8a1cf5f
mohitgoyat525 <montyits71@gmail.com>
2025-02-13 09:27:04 +0530

    add-px on md-screen

--- src/components/Visions.tsx
+++ src/components/Visions.tsx
@@ -4,11 +4,11 @@ import { VISION_LIST } from "@/utils/helper";
 
 const Visions = () => {
   return (
-    <div className="max-lg:px-4 pt-[78px]">
+    <div className="max-xl:px-4 pt-[78px]">
       <div className="flex items-center justify-center max-md:pb-10">
         <CustomHeading textOne="THE VISION" textTwo="THE VISION" />
       </div>
-      <div className="flex items-center justify-center gap-5 max-md:gap-10 flex-wrap mt-[90px] max-md:mt-10 max-xl:justify-start max-md:justify-center">
+      <div className="flex items-center mx-auto justify-center gap-5 max-md:gap-10 flex-wrap mt-[90px] max-md:mt-10 max-lg:justify-start max-md:justify-center">
         {VISION_LIST.map((obj, i) => (
           <div
             key={i}
